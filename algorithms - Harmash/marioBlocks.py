e95a6ecae785e6522c78ed73242d6b74f8bbf372
Mohamedabdeltawab86 <dr.m.tawab2@gmail.com>
2020-12-03 09:27:56 +0200

    submitted via pycharm

--- algorithms - Harmash/marioBlocks.py
+++ algorithms - Harmash/marioBlocks.py
@@ -1,11 +1,13 @@
+#take input from the user limited to inclusive range 1-8
 n = 0
 while n<=0 or n>8:
     n = int(input('Enter a number between 1-8: '))
 
+#simple increasing block
 for i in range(1, n+1):
     print("#" * i)
 
-
+#inverted increasing block
 for i in range(1, n+1):
     print(" " * (n-i), end="")
     print("#" * i, end="")
