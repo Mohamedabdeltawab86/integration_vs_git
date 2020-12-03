9cff4f3d215aca14c821b3fee5b08ac7789abf16
Mohamedabdeltawab86 <dr.m.tawab2@gmail.com>
2020-12-03 10:17:58 +0200

    java file

--- algorithms - Harmash/java code/marioBlock/src/marioBlockSimple.java
+++ algorithms - Harmash/java code/marioBlock/src/marioBlockSimple.java
@@ -17,34 +17,9 @@ public class marioBlockSimple {
         {
             for (int j=1; j<=i; j++)
             {
-                System.out.print("#");
+                System.out.print("#"); //note print function not println
             }
             System.out.println();
         }
     }
 }
-
-//public class marioBlockSimple {
-//
-//    public static void main(String[] args) {
-//
-//        Scanner input = new Scanner(System.in);
-//        int n;
-//
-//        do {
-//            System.out.print("Enter the number of lines: ");
-//            n = input.nextInt();
-//        }
-//        while( n<=0 );
-//
-//        for (int i=1; i<=n; i++)
-//        {
-//            for (int j=1; j<=i; j++)
-//            {
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
-//
-//    }
-//}
