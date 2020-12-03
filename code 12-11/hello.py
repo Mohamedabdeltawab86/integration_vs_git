5ab937beeb64429f09a37242e4efc05f6f9a3564
Mohamedabdeltawab86 <dr.m.tawab2@gmail.com>
2020-12-03 09:17:36 +0200

    submitted via pycharm

--- code 12-11/hello.py
+++
@@ -1,1 +0,0 @@
-print("Hello from vscode to git world!")
---
+++ my work in python/mywork.py
@@ -0,0 +1,39 @@
+#here are some basics in python that I've learn
+#do not worry, one week is enough
+x = 7
+if x == 7:
+    print('OK')  #this is a byway note, that double equal is necessary in python ==
+y = 23
+if x+y>20:
+    print('it is working')
+   
+# lesson (1)printing text, do not forget brackets () and parenthesis ''!
+print ('hello, world')
+# lesson (2)simple operations; in numerical operations, no need for parenthesis''
+    #dividing by zero ==> error, ZeroDivisionError: division by zero
+print(7-6)
+#lesson (3) Floats 1.1, as there is a float, integer transformed to float also.
+print (3/4)
+print (1/3)
+print (1+0.5)
+print (8/4)
+#lesson (4) exponentiation ** quotient and remainder = floor division and modulo //
+print (2**4)
+print (64**0.5)
+print (10**6)
+x=10**6
+'Light velocity'== x 
+print ("Light velocity") #لا تزال هناك مشكلة
+print (20//5)
+print (20//6)
+print (20%6)
+
+#lesson (5) strings '' "" زي بعض, backslash\, newlines\n
+print('Amr\'glass correct \nhis refractive error')
+
+#lesson (6) input function
+#input('hello')
+
+#lesson (7) string operations دمج الأرقام concatenation
+print ("spam"*3)
+
